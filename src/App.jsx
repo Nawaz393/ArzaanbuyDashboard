17ec43dc04d4c7f4f76082d51093cd76b6f94988
mohdnawaz6393@gmail.com <mohdnawaz6393@gmail.com>
2023-05-05 23:07:12 +0500

    adding auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Login } from "@/layouts";
+import { useContext } from "react";
+import { AuthContext } from "./context/authcontext";
 
 import axios from "axios";
 import { SecureRoute } from "./pages/auth/SecureRoute";
 import React from "react";
 function App() {
+  const { authState } = useContext(AuthContext);
   axios.defaults.baseURL = "https://demo-s4hp.onrender.com";
+  console.log(authState.token)
+  const token = JSON.parse(localStorage.getItem("user"))?.token ?? "";
+  axios.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${authState?.token || token}`;
+    return config;
+  });
 
   return (
     <Routes>
