5c6f7e517c43e3eed116a5731106f98b13e95e47
mohdnawaz6393@gmail.com <mohdnawaz6393@gmail.com>
2023-05-05 23:10:55 +0500

    auth

--- src/pages/dashboard/Newuser.jsx
+++ src/pages/dashboard/Newuser.jsx
@@ -1,26 +1,5 @@
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardFooter,
-  Avatar,
-  Typography,
-  Tabs,
-  TabsHeader,
-  Tab,
-  Switch,
-  Tooltip,
-  Button,
-} from "@material-tailwind/react";
-import {
-  HomeIcon,
-  ChatBubbleLeftEllipsisIcon,
-  Cog6ToothIcon,
-  PencilIcon,
-} from "@heroicons/react/24/solid";
-import { Link } from "react-router-dom";
-import { ProfileInfoCard, MessageCard } from "@/widgets/cards";
-import { platformSettingsData, conversationsData, projectsData } from "@/data";
+
+
 import { toast, ToastContainer } from "react-toastify";
 import { useState } from "react";
 import axios from "axios";
